024ada529e54cf9ebcf5200a5cbdc91de683b907
Wojciech Derwisz <derwisz.wojciech@gmail.com>
2025-03-29 16:01:19 +0100

    Created first version of verify_pdf function

--- main_app.py
+++ main_app.py
@@ -8,7 +8,7 @@ from pypdf import PdfReader, PdfWriter
 from getpass import getpass
 
 
-def sign_pdf(pdf_path, private_key_path, output_path, key):
+def create_hash(pdf_path):
     # Wczytanie dokumentu PDF
     with open(pdf_path, "rb") as f:
         pdf_data = f.read()
@@ -17,6 +17,10 @@ def sign_pdf(pdf_path, private_key_path, output_path, key):
     digest = hashes.Hash(hashes.SHA256())
     digest.update(pdf_data)
     pdf_hash = digest.finalize()
+
+    return pdf_hash
+
+def sign_pdf(pdf_path, private_key_path, output_path, key):
 
     # Wczytanie klucza prywatnego
     with open(private_key_path, "rb") as f:
@@ -27,6 +31,7 @@ def sign_pdf(pdf_path, private_key_path, output_path, key):
             print("Podano nieprawidłowy PIN")
             return
 
+    pdf_hash = create_hash(pdf_path)
 
     # Podpisanie dokumentu (RSA-4096)
     signature = private_key.sign(
@@ -53,6 +58,40 @@ def sign_pdf(pdf_path, private_key_path, output_path, key):
 
     print(f"Dokument '{pdf_path}' został podpisany i zapisany jako '{output_path}'.")
 
+
+def verify_pdf(signed_pdf_path, public_key_path):
+
+    reader = PdfReader(signed_pdf_path)
+    writer = PdfWriter()
+
+    # Pobieranie podpisu z pliku PDF
+    signature_hex = reader.metadata.get("/Signature")
+    signature = bytes.fromhex(signature_hex)
+
+    # TODO Usuwanie podpisu z metadanych
+
+
+    signed_pdf_hash = create_hash(signed_pdf_path)
+
+    with open(public_key_path, "rb") as f:
+        public_key = serialization.load_pem_public_key(f.read())
+
+    try:
+        public_key.verify(
+            signature,  # Podpis pobrany z dokumentu PDF
+            signed_pdf_hash,  # Hash obliczony z dokumentu przez użytkownika B
+            padding.PSS(
+                mgf=padding.MGF1(hashes.SHA256()),
+                salt_length=padding.PSS.MAX_LENGTH
+            ),
+            hashes.SHA256()
+        )
+        print("Podpis jest poprawny! Dokument nie został zmieniony.")
+    except:
+        print("Podpis niepoprawny! Dokument mógł zostać zmodyfikowany.")
+
+
+
 # Test: podpisujemy dokument
 
 pin = getpass("Podaj PIN").encode()
@@ -60,3 +99,4 @@ pin = getpass("Podaj PIN").encode()
 key = hashlib.sha256(pin).digest()
 
 sign_pdf("Test PDF.pdf", "private_key.pem", "signed_document.pdf", key)
+#verify_pdf("signed_document.pdf", "public_key.pem")
