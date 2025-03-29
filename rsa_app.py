4f798822793e75b5d6764bcdb90cb6e753eb4d5b
juliababizewska <juliababizewsja@gmail.com>
2025-03-29 13:52:17 +0100

    Updated RSA app, PIN encryption added

--- rsa_app.py
+++ rsa_app.py
@@ -1,5 +1,15 @@
+import hashlib
+from getpass import getpass
+
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
+
+
+#Pobranie PIN-u od użytkownika
+pin = getpass("Podaj PIN:").encode()
+
+#Hashowanie PIN-u
+key = hashlib.sha256(pin).digest()
 
 # Generowanie kluczy
 private_key = rsa.generate_private_key(public_exponent=65537, key_size=4096)
@@ -10,7 +20,7 @@ with open("private_key.pem", "wb") as f:
     f.write(private_key.private_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PrivateFormat.PKCS8,
-        encryption_algorithm=serialization.NoEncryption() #add AES envryption with Users PIN (private key szyfrwany hashem z pinu)
+        encryption_algorithm=serialization.BestAvailableEncryption(key) #Algorytym szyfrowania AES.256
     ))
 
 # Zapis klucza publicznego
@@ -19,3 +29,5 @@ with open("public_key.pem", "wb") as f:
         encoding=serialization.Encoding.PEM,
         format=serialization.PublicFormat.SubjectPublicKeyInfo
     ))
+
+print("Klucze zostały wygenerowanie i zapisane.")
